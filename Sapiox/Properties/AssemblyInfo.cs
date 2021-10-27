7682ac278eeab772ddc458dfe36f559342c1a8a9
Cwaniak <ccwaniaktv@gmail.com>
2021-10-27 19:59:47 +0200

    update

--- Sapiox/Properties/AssemblyInfo.cs
+++ Sapiox/Properties/AssemblyInfo.cs
@@ -1,10 +1,10 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Fedora")]
+[assembly: AssemblyTitle("Sapiox")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyCompany("Fedora-DevTeam")]
+[assembly: AssemblyCompany("SapioxSL")]
 [assembly: AssemblyProduct("Fedora")]
-[assembly: AssemblyCopyright("Copyright © Fedora-DevTeam 2021")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyCopyright("Copyright © SapioxSL 2021")]
+[assembly: AssemblyVersion("0.0.6")]
+[assembly: AssemblyFileVersion("0.0.6")]
